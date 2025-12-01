21f0903658d718cbedfd62c2f1b62d4b0568f16c
nimurr <nimurnerob404@gmail.com>
2025-12-01 09:52:47 +0600

    first commit

---
+++ src/Components/Common/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='bg-black text-white py-5 text-xl text-center '>
+            Header
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/app/(main)/page.js
@@ -0,0 +1,31 @@
+'use client';
+import Loading from '@/Components/others/Loading';
+import { useGetDemoDataQuery } from '@/redux/fetures/Demo/demoDataGet';
+import React from 'react';
+
+const Page = () => {
+
+    const { data, isLoading } = useGetDemoDataQuery();
+    console.log(data)
+
+    return (
+        <div className='container mx-auto'>
+            <h2 className='text-3xl font-semibold bg-primary text-center my-10 rounded-md py-2'>Demo Api Data Get For Check Rediux </h2>
+            <div className='grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4'>
+                {
+                    data?.map((item) => (
+                        <div className='bg-purple-500 hover:bg-purple-600 rounded-lg p-5 text-white' key={item.id}>
+                            <h1 className='font-semibold mb-2 capitalize'>{item.title}</h1>
+                            <p className='text-gray-300 capitalize'>{item.body}</p>
+                        </div>
+                    ))
+                }
+                {
+                    isLoading && <Loading item={10} line={5} />
+                }
+            </div>
+        </div>
+    );
+}
+
+export default Page;
