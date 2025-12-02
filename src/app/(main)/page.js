588829db4c59405d7da3395b0ad99512903e9e1e
nimurr <nimurnerob404@gmail.com>
2025-12-02 12:09:01 +0600

    `Removed images and added Hero component to page.js`

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,10 +1,11 @@
 'use client';
+import Hero from '@/Components/Home/Hero';
 import React from 'react';
 const Page = () => {
 
     return (
         <div className='container mx-auto bg-primary min-h-[10px]'>
-            
+            <Hero />
         </div>
     );
 }
